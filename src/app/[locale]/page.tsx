58baa63fd638a3e05e244711afd1c7a1a359b4b1
youssef <youssefhsn16@gmail.com>
2025-02-09 11:35:10 +0200

    🛠️ Refactor(HomePage) Remove Container wrapper for simplified layout

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,13 @@
-import { Container } from '@mui/material';
 import HeroSection from '@/components/Home/HeroSection';
 import AboutSection from '@/components/Home/AboutSection';
 import ContactSection from '@/components/Home/ContactSection';
 
 export default function HomePage({ params }: { params: { locale: string } }) {
   return (
-    <Container maxWidth="xl" disableGutters>
+    <>
       <HeroSection/>
       <AboutSection/>
       <ContactSection/>
-    </Container>
+    </>
   );
 }
