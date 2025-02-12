098717552ea880c7cd57edb156d7903f20d3b451
youssef <youssefhsn16@gmail.com>
2025-02-12 14:11:48 +0200

    🛠️ Fix(Typos)

--- src/components/Home/ServicesSection.tsx
+++ src/components/Home/ServicesSection.tsx
@@ -3,14 +3,14 @@ import { Flight } from '@mui/icons-material';
 import { useTranslations } from 'next-intl';
 import ServiceCard from '@/components/Cards/ServiceCard';
 
-export default function AboutSection() {
+export default function ServicesSection() {
   const t = useTranslations("services");
 
   return (
     <Box component="section" id="services">
       <Typography variant="h3">{t('title')}</Typography>
       <Typography>{t('description')}</Typography>
-      <Container component="ul" sx={{display: 'flex', alignItems: 'center', justifyContent: 'space-around', listStyle: 'none', p: 0, m: 0, ">li": {m: 0, listStyle: 'none', padding: 0}}}>
+      <Container component="ul" sx={{display: 'flex', alignItems: 'center', justifyContent: 'space-around', listStyle: 'none', p: 0, mb: 0, ">li": {m: 0, listStyle: 'none', padding: 0}}}>
         <ServiceCard icon={<Flight/>} name={t("flights.title")} brief={t("flights.description")}/>
         <ServiceCard icon={<Flight/>} name={t("work.title")} brief={t("work.description")}/>
         <ServiceCard icon={<Flight/>} name={t("tours.title")} brief={t("tours.description")}/>
